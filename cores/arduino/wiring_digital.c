2125d8a08f01bf1ba50121d0539b378e003df411
Mydhily M R <mydhily37@gmail.com>
2022-06-30 14:19:16 +0530

    vega-bsp

--- cores/arduino/wiring_digital.c
+++ cores/arduino/wiring_digital.c
@@ -4,24 +4,185 @@
 
 __BEGIN_DECLS
 
-void
-pinMode(uint32_t pin, uint32_t mode)
+/**
+
+ @file wiring_digital.c
+
+ @brief Contains routines to read/write GPIO pins
+
+ */
+
+
+void pinMode(uint32_t pin, uint32_t mode)
 {
-  
-}
+	unsigned char gpio_number = 0;
+	unsigned short dir_data = 0;
+	unsigned short bit_position = 0;
+	volatile unsigned short *gpio_0_dir_addr = (volatile unsigned short *)(GPIO_BASE_ADDR +0x40000) ; // GPIO 0 direction register.
+	volatile unsigned short *gpio_1_dir_addr = (volatile unsigned short *)(GPI1_BASE_ADDR +0x40000) ; // GPIO 1 direction register.
+
+	unsigned long gen_gpio_0_addr,gen_gpio_1_addr;
+	volatile unsigned short *gpio_0_data, *gpio_1_data;
+	unsigned short read_data = 0;
+
+	if(pin <= 15)
+	      gpio_number = 0;             // Pins 0 to 15 defined in GPIO 0.
+	else
+	      gpio_number = 1;            // Pins 16 to 31 defined in GPIO 1.
+
+	if(mode == 0)
+	{
+	if(gpio_number == 0)
+	{
+		bit_position = (1 << pin);        // Align the selected pin to its position.
+	    dir_data =  *gpio_0_dir_addr;         // Address of the direction register.
+	    dir_data &= ~(bit_position);        // Clearing a bit configures the pin to be INPUT.
+	    *gpio_0_dir_addr = dir_data;        // Data written to direction register.
+	    __asm__ __volatile__ ("fence");
+	 }
+
+	else if(gpio_number == 1)
+	{
+		pin = (pin - 16);
+	    bit_position = (1 << pin);        // Align the selected pin to its position.
+	    dir_data =  *gpio_1_dir_addr;        // Address of the direction register.
+	    dir_data &= ~(bit_position);        // Clearing a bit configures the pin to be INPUT.
+	    *gpio_1_dir_addr = dir_data;        // Data written to direction register.
+	     __asm__ __volatile__ ("fence");
+	 }
+	 }
+
+	else if(mode == 1)
+	{
+	if(gpio_number == 0)
+	{
+		bit_position = (1 << pin);            // Align the selected pin to its position.
+	    dir_data =  *gpio_0_dir_addr;             // Address of the direction register.
+	    dir_data |= bit_position;            //Setting a bit configures the pin to be OUTPUT.
+	    *gpio_0_dir_addr = dir_data;            // Data written to direction register.
+	     __asm__ __volatile__ ("fence");
+	 }
+
+	else if(gpio_number == 1)
+	{
+		pin = (pin - 16);
+	    bit_position = (1 << pin);            // Align the selected pin to its position.
+	    dir_data =  *gpio_1_dir_addr;            // Address of the direction register.
+	    dir_data |= bit_position;            //Setting a bit configures the pin to be OUTPUT.
+	    *gpio_1_dir_addr = dir_data;            // Data written to direction register.
+	     __asm__ __volatile__ ("fence");
+	}
+
+	}
+	     __asm__ __volatile__ ("fence");
+	}
 
 
-void
-digitalWrite(uint32_t pin, uint32_t val)
+
+void digitalWrite(uint32_t pin, uint32_t val)
 {
- 
+	unsigned char gpio_number = 0;
+    unsigned short dir_data = 0;
+	unsigned short bit_position = 0;
+	volatile unsigned short *gpio_0_dir_addr = (volatile unsigned short *)(GPIO_BASE_ADDR +0x40000) ; // GPIO 0 direction register.
+	volatile unsigned short *gpio_1_dir_addr = (volatile unsigned short *)(GPI1_BASE_ADDR +0x40000) ; // GPIO 1 direction register.
 
-}
+	unsigned long gen_gpio_0_addr,gen_gpio_1_addr;
+	volatile unsigned short *gpio_0_data, *gpio_1_data;
 
-int
-digitalRead(uint32_t pin)
+	if(pin <= 15)
+		gpio_number = 0;			// Pins 0 to 15 defined in GPIO 0.
+	else
+		gpio_number = 1;			// Pins 16 to 31 defined in GPIO 1.
+
+
+	if(gpio_number == 0)
+	{
+		bit_position = (1 << pin);			// Align the selected pin to its position.
+		dir_data =  *gpio_0_dir_addr;	 		// Address of the direction register.
+		dir_data |= bit_position;			//Setting a bit configures the pin to be OUTPUT.
+		*gpio_0_dir_addr = dir_data;			// Data written to direction register.
+		__asm__ __volatile__ ("fence");
+
+		gen_gpio_0_addr = GPIO_BASE_ADDR;		// Base address of GPIO 0.
+		gen_gpio_0_addr|=(bit_position << 2);		// Generate address of the selected GPIO pin in GPIO 0.
+		gpio_0_data = (unsigned short *)gen_gpio_0_addr;		// Generated address.
+		*gpio_0_data = (val << pin);		// Write data to the address.
+	}
+
+
+	else if(gpio_number == 1)
+	{
+		pin = (pin - 16);
+		bit_position = (1 << pin);			// Align the selected pin to its position.
+		dir_data =  *gpio_1_dir_addr;			// Address of the direction register.
+		dir_data |= bit_position;			//Setting a bit configures the pin to be OUTPUT.
+		*gpio_1_dir_addr = dir_data;			// Data written to direction register.
+		__asm__ __volatile__ ("fence");
+
+		gen_gpio_1_addr = GPI1_BASE_ADDR;		// Base address of GPIO 1.
+		gen_gpio_1_addr|=(bit_position << 2);		// Generate address of the selected GPIO pin in GPIO 0.
+		gpio_1_data = (unsigned short *)gen_gpio_1_addr;		// Generated address.
+		*gpio_1_data = (val << pin);		// Write data to the address.
+	}
+		__asm__ __volatile__ ("fence");
+
+		return;
+	}
+
+
+int digitalRead(uint32_t pin)
 {
+	unsigned char gpio_number = 0;
+	unsigned short dir_data = 0;
+	unsigned short bit_position = 0;
 
+	volatile unsigned short *gpio_0_dir_addr = (volatile unsigned short *)(GPIO_BASE_ADDR +0x40000) ; // GPIO 0 direction register.
+	volatile unsigned short *gpio_1_dir_addr = (volatile unsigned short *)(GPI1_BASE_ADDR +0x40000) ; // GPIO 1 direction register.
+	unsigned long gen_gpio_0_addr,gen_gpio_1_addr;
+
+	volatile unsigned short *gpio_0_data, *gpio_1_data;
+	unsigned short read_data = 0;
+
+	if(pin <= 15)
+		gpio_number = 0; 			// Pins 0 to 15 defined in GPIO 0.
+	else
+		gpio_number = 1;			// Pins 16 to 31 defined in GPIO 1.
+
+
+	if(gpio_number == 0)
+	{
+		bit_position = (1 << pin);		// Align the selected pin to its position.
+		dir_data =  *gpio_0_dir_addr; 		// Address of the direction register.
+		dir_data &= ~(bit_position);		// Clearing a bit configures the pin to be INPUT.
+		*gpio_0_dir_addr = dir_data;		// Data written to direction register.
+		__asm__ __volatile__ ("fence");
+
+		gen_gpio_0_addr = GPIO_BASE_ADDR;	// Base address of GPIO 0.
+		gen_gpio_0_addr+=(bit_position << 2);	// Generate address of the selected GPIO pin in GPIO 0.
+		gpio_0_data = (unsigned short *)gen_gpio_0_addr;	// Generated address.
+		read_data = *gpio_0_data; 		// Read data from the address.
+	}
+
+	else if(gpio_number == 1)
+	{
+	pin = (pin - 16);
+	bit_position = (1 << pin);		// Align the selected pin to its position.
+	dir_data =  *gpio_1_dir_addr;		// Address of the direction register.
+	dir_data &= ~(bit_position);		// Clearing a bit configures the pin to be INPUT.
+	*gpio_1_dir_addr = dir_data;		// Data written to direction register.
+	__asm__ __volatile__ ("fence");
+
+	gen_gpio_1_addr = GPI1_BASE_ADDR;	// Base address of GPIO 1.
+	gen_gpio_1_addr+=(bit_position << 2);	// Generate address of the selected GPIO pin in GPIO 1.
+	gpio_1_data = (unsigned short *)gen_gpio_1_addr;	// Generated address.
+	read_data = *gpio_1_data;		// Read data from the address.
+	}
+	__asm__ __volatile__ ("fence");
+
+	if(read_data)
+		return 1; 				// GPIO pin is High.
+	else
+		return 0; 				// GPIO pin is Low.
 }
-
 __END_DECLS
--- cores/arduino/wiring_digital.h
+++ cores/arduino/wiring_digital.h
@@ -21,16 +21,62 @@
 
 __BEGIN_DECLS
 
-#define PortValue_t uint32_t
-#define PortAddr_t uint32_t
-#define PortRegister_t volatile uint32_t *
+/***************************************************/
+#define PIN_0			0
+#define PIN_1			1
+#define PIN_2			2
+#define PIN_3			3
 
-#define digitalPinToPort(pin)    ((PortAddr_t)     GPIO_BASE_ADDR)
-#define digitalPinToPortIn(pin)  ((PortAddr_t)     GPIO_BASE_ADDR)
-#define digitalPinToBitMask(pin) ((PortValue_t)    (1<<variant_pin_map[pin].bit_pos))
-#define portOutputRegister(port) ((PortRegister_t) (GPIO_BASE_ADDR + GPIO_OUTPUT_VAL))
-#define portInputRegister(port)  ((PortRegister_t) (GPIO_BASE_ADDR + GPIO_INPUT_VAL))
+#define PIN_4			4
+#define PIN_5			5
+#define PIN_6			6
+#define PIN_7			7
 
+#define PIN_8			8
+#define PIN_9			9
+#define PIN_10			10
+#define PIN_11			11
+
+#define PIN_12			12
+#define PIN_13			13
+#define PIN_14			14
+#define PIN_15			15
+
+#define PIN_16			16
+#define PIN_17			17
+#define PIN_18			18
+#define PIN_19			19
+
+#define PIN_20			20
+#define PIN_21			21
+#define PIN_22			22
+#define PIN_23			23
+
+#define PIN_24			24
+#define PIN_25			25
+#define PIN_26			26
+#define PIN_27			27
+
+#define PIN_28			28
+#define PIN_29			29
+#define PIN_30			30
+#define PIN_31			31
+
+#define GPIO_0			0
+#define GPIO_1			1
+#define GPIO_2			2
+#define GPIO_3			3
+
+
+#define ON_LED			0
+#define OFF_LED			1
+
+
+#define ON			1
+#define OFF			0
+
+
+/************************************************************************************************/
 /**
  * \brief Configures the specified pin to behave either as an input or an output. See the description of digital pins for details.
  *
