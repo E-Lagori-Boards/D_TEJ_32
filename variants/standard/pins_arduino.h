cf134365b3d3fc415982978308d88183d61fb392
HimanshuDiwane <kishor.dh@cdac.in>
2022-08-04 17:59:20 +0530

    pins_arduino.h file updated

--- variants/standard/pins_arduino.h
+++ variants/standard/pins_arduino.h
@@ -1,3 +1,16 @@
 // API compatibility
 #include "variant.h"
 
+#define PINS_COUNT    (40u)
+
+#define NINA_CTS      NINA_ACK
+#define NINA_RTS      NINA_GPIO0
+
+#define PIN_LED_13  (13u)
+#define PIN_LED     PIN_LED_13
+#define LED_BUILTIN PIN_LED
+
+//NINA
+#define NINA_GPIO0  (1) //connected to gpio1 of aries
+#define NINA_RESETN (27u)
+#define NINA_ACK    (28u)
