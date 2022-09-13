76ad4050ef3180241eb6af69ef1baf2c0962802d
HimanshuDiwane <kishor.dh@cdac.in>
2022-09-13 12:18:51 +0530

    pins_arduino.h file modified

--- variants/standard/pins_arduino.h
+++ variants/standard/pins_arduino.h
@@ -14,3 +14,17 @@
 #define NINA_GPIO0  (13) //connected to GPIO13 of aries
 #define NINA_RESETN (27u)  //Not using
 #define NINA_ACK    (28u)   //Not using
+
+// #define SS 1
+#define SS_FLASHMEM 1
+
+#define PIN_SPI_SS    (10)
+#define PIN_SPI_MOSI  (16)
+#define PIN_SPI_MISO  (14)
+#define PIN_SPI_SCK   (15)
+#define SDCARD_SS_PIN (10)
+
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
