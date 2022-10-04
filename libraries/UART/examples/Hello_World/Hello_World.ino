13244c744e1d33a361b9cd789c0cdb17a142f1d1
Mydhily M R <mydhily37@gmail.com>
2022-10-04 09:44:04 +0530

    UART header file removed

--- libraries/UART/examples/Hello_World/Hello_World.ino
+++ libraries/UART/examples/Hello_World/Hello_World.ino
@@ -10,18 +10,15 @@
 
    .
  **/
-#include <UARTClass.h>
 
 void setup() {
   // put your setup code here, to run once:
-  //UARTClass sensors(0);
   Serial.begin(115200);
-  Serial.println("Hello World!");
 }
 
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World!...");
+  Serial.println("Hello World...!");
 }
