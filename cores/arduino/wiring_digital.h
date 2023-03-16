fc7986c138891745b4ed6d7c46b7ec9de1100341
Himanshu Diwane <himanshudiwane@gmail.com>
2023-03-16 11:39:51 +0530

    digitalRead() added for ARIES IoT

--- cores/arduino/wiring_digital.h
+++ cores/arduino/wiring_digital.h
@@ -22,12 +22,12 @@
 __BEGIN_DECLS
 
 /***************************************************/
-#define PIN_0			0
-#define PIN_1			1
-#define PIN_2			2
-#define PIN_3			3
+// #define PIN_0			0
+// #define PIN_1			1
+// #define PIN_2			2
+// #define PIN_3			3
 
-#define PIN_4			4
+// #define PIN_4			4
 #define PIN_5			5
 #define PIN_6			6
 #define PIN_7			7
